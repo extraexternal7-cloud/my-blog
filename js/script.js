57fbdbb76a9e70e1c252d7d045c424f1d1f40265
Extra External <extraexternal7@gmail.com>
2026-01-20 19:48:40 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+// Auto Date
+document.getElementById("date").innerText =
+  new Date().toDateString();
+
+// Dark Mode Toggle
+function toggleDark() {
+  document.body.classList.toggle("dark");
+}
+
 console.log("Blog site loaded successfully!");
+
